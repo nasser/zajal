a1e47d21e85fa359c8c4c02099f89cacb3bfa196
Ramsey Nasser <ram@nas.sr>
2011-02-02 08:26:33 -0500

    added noise and signed_noise to Mathematics

--- src/mathematics.c
+++ src/mathematics.c
@@ -52,6 +52,58 @@ VALUE zj_random_height(VALUE self) {
   return DBL2NUM(ofRandomHeight());
 }
 
+VALUE zj_noise(int argc, VALUE* argv, VALUE klass) {
+  VALUE x, y, z, w;
+  rb_scan_args(argc, argv, "13", &x, &y, &z, &w);
+  
+  if(!NIL_P(y) && !NIL_P(z) && !NIL_P(w)) {
+    /* called with four arguments, return 4D noise */
+    return DBL2NUM(ofNoise(NUM2DBL(x), NUM2DBL(y), NUM2DBL(z), NUM2DBL(w)));
+    
+  } else if(!NIL_P(y) && !NIL_P(z) && NIL_P(w)) {
+    /* called with three arguments, return 3D noise */
+    return DBL2NUM(ofNoise(NUM2DBL(x), NUM2DBL(y), NUM2DBL(z)));
+    
+  } else if(!NIL_P(y) && NIL_P(z) && NIL_P(w)) {
+    /* called with two arguments, return 2D noise */
+    return DBL2NUM(ofNoise(NUM2DBL(x), NUM2DBL(y)));
+    
+  } else if(NIL_P(y) && NIL_P(z) && NIL_P(w)) {
+    /* called with one argument, return 1D noise */
+    /* TODO calling ofNoise with one parameter always returns 0.5, why? */
+    return DBL2NUM(ofNoise(NUM2DBL(x), 0));
+    
+  }
+  
+  return Qnil;
+}
+
+VALUE zj_signed_noise(int argc, VALUE* argv, VALUE klass) {
+  VALUE x, y, z, w;
+  rb_scan_args(argc, argv, "13", &x, &y, &z, &w);
+  
+  if(!NIL_P(y) && !NIL_P(z) && !NIL_P(w)) {
+    /* called with four arguments, return 4D noise */
+    return DBL2NUM(ofSignedNoise(NUM2DBL(x), NUM2DBL(y), NUM2DBL(z), NUM2DBL(w)));
+    
+  } else if(!NIL_P(y) && !NIL_P(z) && NIL_P(w)) {
+    /* called with three arguments, return 3D noise */
+    return DBL2NUM(ofSignedNoise(NUM2DBL(x), NUM2DBL(y), NUM2DBL(z)));
+    
+  } else if(!NIL_P(y) && NIL_P(z) && NIL_P(w)) {
+    /* called with two arguments, return 2D noise */
+    return DBL2NUM(ofSignedNoise(NUM2DBL(x), NUM2DBL(y)));
+    
+  } else if(NIL_P(y) && NIL_P(z) && NIL_P(w)) {
+    /* called with one argument, return 1D noise */
+    /* TODO calling ofSignedNoise with one parameter always returns 0.5, why? */
+    return DBL2NUM(ofSignedNoise(NUM2DBL(x), 0));
+    
+  }
+  
+  return Qnil;
+}
+
 /* methods added to Numeric class */
 
 VALUE zj_to_deg(VALUE self) {
@@ -86,6 +138,9 @@ void Init_Mathematics() {
   rb_define_method(zj_mMathematics, "random", RB_FUNC(zj_random), -1);
   rb_define_method(zj_mMathematics, "random_width", RB_FUNC(zj_random_width), 0);
   rb_define_method(zj_mMathematics, "random_height", RB_FUNC(zj_random_height), 0);
+  
+  rb_define_method(zj_mMathematics, "noise", RB_FUNC(zj_noise), -1);
+  rb_define_method(zj_mMathematics, "signed_noise", RB_FUNC(zj_signed_noise), -1);
   
   rb_define_const(zj_mMathematics, "Pi", DBL2NUM(PI));
   rb_define_const(zj_mMathematics, "TwoPi", DBL2NUM(TWO_PI));
