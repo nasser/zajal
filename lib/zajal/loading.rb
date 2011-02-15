38de210f58ab77c5183092c5f9a76c4a30dea96c
Ramsey Nasser <ram@nas.sr>
2011-02-14 22:40:02 -0500

    got rid of some old code

--- lib/zajal/loading.rb
+++ lib/zajal/loading.rb
@@ -64,31 +64,6 @@ end
 
 # syntax nodes that change the scope
 ScopeChangers = [:def, :method_add_block, :class, :module]
-EventBlocks = %w(setup update draw exit window_resized key_pressed key_released mouse_moved mouse_dragged mouse_pressed mouse_released audio_requested audio_received)
-
-# reduce to assigns, pull out assigns in top level scope of exp
-ToAssigns = proc { |arr, exp|
-  if exp.respond_to? :first and exp.first == :assign
-    arr << exp[1][1][1]
-  elsif exp.respond_to? :first and exp.first == :massign
-    exp[1].each { |asn| arr << asn[1] }
-  elsif exp.respond_to? :reduce and not ScopeChangers.include? exp.first
-    exp.reduce(arr, &ToAssigns)
-  end
-
-  arr
-}
-
-# reduce to methods, pull out method invocations from exp
-ToMethods = proc { |arr, exp|
-  if exp.respond_to? :first and exp.first == :method_add_block
-    arr << exp[1][1][1][1]
-  elsif exp.respond_to? :reduce
-    exp.reduce(arr, &ToMethods)
-  end
-  
-  arr
-}
 
 # Make all top level locals global. Implements Processing style syntax and
 # avoids having to write $ signs everywhere
@@ -106,6 +81,19 @@ def globalize_code code, sigil="$"
   line_inserts = {}
   code_ary = code.each_line.to_a
   
+  # reduce to assigns, pull out assigns in top level scope of exp
+  to_assigns = proc { |arr, exp|
+    if exp.respond_to? :first and exp.first == :assign
+      arr << exp[1][1][1]
+    elsif exp.respond_to? :first and exp.first == :massign
+      exp[1].each { |asn| arr << asn[1] }
+    elsif exp.respond_to? :reduce and not ScopeChangers.include? exp.first
+      exp.reduce(arr, &to_assigns)
+    end
+
+    arr
+  }
+
   # globalize one identifier in the code
   globalize_one_ident = proc do |ident, exp|
     if exp.respond_to? :first and exp.first == :@ident and exp[1] == ident
@@ -124,7 +112,7 @@ def globalize_code code, sigil="$"
   end
   
   # extract top level locals to be globalized
-  top_level_locals = sexp.reduce([], &ToAssigns)
+  top_level_locals = sexp.reduce([], &to_assigns)
   
   # globalize each top level local
   top_level_locals.each { |l| globalize_one_ident.call l, sexp }
@@ -134,26 +122,6 @@ end
 
 def valid? code
   not Ripper.sexp(code).nil?
-end
-
-# Compare two scripts as Zajal code and determine if a full reset it required
-# 
-# local_code - Code currently running
-# other_code - Incoming code
-# 
-# Returns true if the code differs enough to warrant a full reset, false otherwise
-def compare_code local_code, other_code
-  local_sexp = Ripper.sexp_simple(local_code)[1]
-  other_sexp = Ripper.sexp_simple(other_code)[1]
-  
-  # figure out what was added and removed
-  removed = local_sexp - other_sexp
-  added   = other_sexp - local_sexp
-  
-  modified_globals = removed.reduce([], &ToAssigns) | added.reduce([], &ToAssigns)
-  modified_methods = removed.reduce([], &ToAssigns) | added.reduce([], &ToMethods)
-  
-  modified_methods.include?("setup") or not modified_globals.empty?
 end
 
 def capture_state
@@ -173,22 +141,6 @@ def capture_state
   end
 end
 
-# Determing if code is in reduced mode or not.
-# Reduced mode is Zajal code that does not use any event blocks.
-# 
-# code - A ruby String containing the code to analyze
-# 
-# Returns true if code is in reduced mode, false otherwise
-def reduced_mode? code
-  not Ripper.sexp_simple(code)[1].reduce([], &ToMethods).include? EventBlocks
-end
-
-def try_eval code
-  begin
-    eval code
-  rescue 
-    return false
-  end
   
   return true
 end
