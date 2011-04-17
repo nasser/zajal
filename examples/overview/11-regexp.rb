c176b7c42f9c6e9ddf48a80fcdf6dc2616c3ccda
Ramsey Nasser <ram@nas.sr>
2011-04-16 22:32:30 -0400

    pulled out a lot of old examples

--- examples/overview/11-regexp.rb
+++
@@ -1,9 +0,0 @@
-# size 400, 200
-# text "http://www.ruby-doc.org/core/classes/Regexp.html"
-# 
-# text /hello/
-# 
-# text "hello" =~ /hell/
-# text "hello" =~ /ll/
-# text "hello" =~ /spam/
-# text "hello" =~ /[a-z]*/
