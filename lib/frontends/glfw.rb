7350fa19afc00c24cee72e6b134b88d45652090d
Ramsey Nasser <ram@nas.sr>
2012-09-20 12:10:24 -0400

    Start Zajal::Frontends::Glfw documentation

--- lib/frontends/glfw.rb
+++ lib/frontends/glfw.rb
@@ -1,14 +1,39 @@
 module Zajal
 	module Frontends
+		# The basic, command-line friendly frontend.
+		# 
+		# This is a replacement for the GLUT based windows that openFrameworks
+		# uses. GLFW is much easier to work with than GLUT, and doesn't hog
+		# the main loop.
+		# 
+		# @api internal
 		class Glfw
+			# The sketch to render
+			# 
+			# A sketch must be set before {#run} is called.
 			attr_accessor :sketch
 
-			def initialize w, h
+			# Create a new Glfw frontend, and open a window with size +(width,height)+
+			# 
+			# @param width [Numeric] width of the window
+			# @param height [Numeric] height of the window
+			# 
+			# @todo Solve initial window size problem
+			def initialize width, height
 				@window = Native.glfwfrontend_new
-				@w, @h = w, h
-				Zajal::Graphics::Native.ofSetupOpenGL @window, w, h, 0
+				Zajal::Graphics::Native.ofSetupOpenGL @window, width, height, 0
 			end
 
+			# Run the sketch
+			# 
+			# Kicks off setup/update/draw loop for the attached sketch
+			# 
+			# @example
+			# 	glfw = Zajal::Frontends::Glfw.new 100, 100
+			# 	glfw.sketch = Zajal::Sketch.new "user.zj"
+			# 	glfw.run
+			# 
+			# @return [nil] Nothing
 			def run
 				@sketch.setup
 				while true do
