a9e234f55cb007bd39a46b0233071422db97f5a8
Ramsey Nasser <ram@nas.sr>
2012-09-20 12:07:21 -0400

    Start Zajal::Version documentation

--- lib/core/version.rb
+++ lib/core/version.rb
@@ -1,8 +1,16 @@
 module Zajal
 	module Version
+		# Major version number, the X in X.Y.Z
 		MAJOR 		= 0
+
+		# Minor version number, the Y in X.Y.Z
 		MINOR 		= 4
+
+		# Revision number, the Z in X.Y.Z
 		REVISION 	= 0
+
+		# Release code name, named after the streets where Zajal is being
+		# developed
 		NAME 		= "Amsterdam"
 
 		def self.print
@@ -12,5 +20,8 @@ module Zajal
 		end
 	end
 
+	# Human readable version string
+	# @example
+	# 	VERSION # => "Zajal 0.4.0 Amsterdam"
 	VERSION = "Zajal - #{Version::MAJOR}.#{Version::MINOR}.#{Version::REVISION} #{Version::NAME}"
 end
