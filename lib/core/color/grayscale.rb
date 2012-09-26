871210a19724e57d41778621de47fb411ead1f3a
Clinton Judy <clinton@j-udy.com>
2012-09-26 00:25:57 -0400

    Add grayscale colors.

---
+++ lib/core/color/grayscale.rb
@@ -0,0 +1,20 @@
+class Color::Grayscale
+  attr_accessor :level, :a
+
+  # Create a new Color::Grayscale object. 
+  # 
+  # @param level [Numeric] the shade of gray from black to white, 0..255
+  # 
+  # @return [Color::Grayscale] a new Color::Grayscale instance
+  def initialize(level=0, a=0)
+    @level = level
+    @a = a
+  end
+
+  # Returns a Color::RGB instance in this shade of gray.
+  # 
+  # @return [Color::RGB]
+  def to_rgb
+    Color::RGB.new(@level, @level, @level, @a)
+  end
+end
---
+++ spec/core/color/grayscale_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../../spec_helper'
+require_relative '../../../lib/core/color/grayscale'
+require_relative '../../../lib/core/color/rgb'
+
+describe Color::Grayscale do
+  describe "#to_rgb" do
+    it "should create a new Color::RGB instance at the right grayscale value" do
+      Color::Grayscale.new(100, 40).to_rgb.to_a.should == [100, 100, 100, 40]
+    end
+  end
+end
