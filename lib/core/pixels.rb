152fc9f0f1500b6fd786668387950781f27851e9
Ramsey Nasser <ram@nas.sr>
2012-11-01 01:27:12 -0400

    Fix Pixels#save cwd bug
    This should be fixed in openFrameworks

--- lib/core/pixels.rb
+++ lib/core/pixels.rb
@@ -16,9 +16,12 @@ module Zajal
         end
       end
 
+      # TODO fix cwd bug in ofSaveImage!!
+      # @todo fix cwd bug in ofSaveImage!!
       def save path, quality=:best
+        path = File.expand_path(path.to_s)
         Dir.chdir do
-          Native.ofSaveImage @pointer, File.expand_path(path.to_s).to_ptr, quality
+          Native.ofSaveImage @pointer, path.to_ptr, quality
         end
       end
 
