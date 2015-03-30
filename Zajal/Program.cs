b6027bce33b0d5a0eef3be8b90dc72f8c89dac5d
Ramsey Nasser <ram@nas.sr>
2015-03-30 00:40:38 -0400

    Better print outs

--- Zajal/Program.cs
+++ Zajal/Program.cs
@@ -6,18 +6,23 @@ using System.Threading;
 using System.IO;
 
 using clojure.lang;
-using OpenTK;
 
 namespace Zajal {
 	public class Program {
 		[DllImport("__Internal", EntryPoint="mono_get_runtime_build_info")]
 		public extern static string GetMonoVersion();
 
+		public static int VersionMajor = 0;
+		public static int VersionMinor = 5;
+		public static int VersionRevision = 0;
+		public static string VersionName = "Nostrand";
+		public static string Version { get { return VersionMajor + "." + VersionMinor + "." + VersionRevision + " - " + VersionName; } }
+
 		public static event Func<object> MainThreadLoop;
 
 		public static void SetClojureClassPath(string path) {
 			Environment.SetEnvironmentVariable("CLOJURE_LOAD_PATH", path);
-			Console.WriteLine (path);
+			Console.WriteLine ("Set CLOJURE_LOAD_PATH to " + path);
 		}
 
 		public static void SetClojureClassPath() {
@@ -25,9 +30,15 @@ namespace Zajal {
 		}
 
 		public static void Main () {
+			Console.WriteLine ("Zajal " + Program.Version);
+			Console.WriteLine ("Mono " + GetMonoVersion());
+
 			SetClojureClassPath ();
 
+			Console.Write ("Loading Clojure...");
 			RT.load ("zajal/repl");
+			Console.WriteLine ("OK");
+
 			((IFn)RT.var("zajal.repl", "start-server").getRawRoot()).invoke(11211);
 			IFn ZajalReplEvalFn = (IFn)RT.var("zajal.repl", "eval-queue").getRawRoot();
 
