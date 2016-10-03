fbbec6085bb8f5f00c7ec8f806e69f6737c6b17b
Ramsey Nasser <ram@nas.sr>
2016-10-02 22:06:59 -0400

    pass input to step function

--- figwheel-browser/src/zajal/core.cljs
+++ figwheel-browser/src/zajal/core.cljs
@@ -3,12 +3,36 @@
 
 (enable-console-print!)
 
-(defn sketch [start update draw]
+(defonce start-time (js/Date.now))
+
+(defonce mouse-state (atom {:x 0 :y 0 :pressed false}))
+
+(set! js/document.onmousemove
+      #(swap! mouse-state
+             assoc
+             :x (.-pageX %)
+             :y (.-pageY %)))
+
+(set! js/document.onmousedown
+      #(swap! mouse-state
+              assoc :pressed? true))
+
+(set! js/document.onmouseup
+      #(swap! mouse-state
+              assoc :pressed? false))
+
+(defn input []
+  (let [t (js/Date.now)]
+    {:time {:elapsed (- t start-time)}
+     :mouse @mouse-state }))
+
+(defn sketch [start step draw]
+  (step {} (input))
   (let [state (reagent/atom start)]
     (letfn [(component [] [draw @state])
             (render-loop [t]
                          (.begin js/stats)
-                         (swap! state update)
+                         (swap! state step (input))
                          (.end js/stats)
                          (.requestAnimationFrame js/window render-loop))]
       (reagent/render-component
