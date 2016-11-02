eed27dc5f9173a38a5fd719ebc5194c5c7fbcaff
Ramsey Nasser <ram@nas.sr>
2016-11-02 14:49:22 -0400

    reorganize

--- electron/hello.cljs
+++ electron/hello.cljs
@@ -1,66 +1,19 @@
 (ns hello
-  (:require there))
-
-(defn renderer* [parent body]
-  (js/window.VirtualDom.renderer parent nil body))
-
-(defn text* [s]
-  (js/window.VirtualDom.text s))
-
-(defn node [tag attrs children]
-  (js/window.VirtualDom.node tag (clj->js attrs) (clj->js children)))
-
-;; input
-
-(defonce start-time (js/Date.now))
-(defonce mouse-state (atom {:x 0 :y 0 :pressed false}))
-
-(js/window.document.addEventListener
-  "mousemove"
-  #(swap! mouse-state
-          assoc
-          :x (.-pageX %)
-          :y (.-pageY %)))
-
-(js/window.document.addEventListener
-  "mousedown"
-  #(swap! mouse-state
-          assoc :pressed? true))
-
-(js/window.document.addEventListener
-  "mouseup"
-  #(swap! mouse-state
-          assoc :pressed? false))
-
-(defn input []
-  (let [t (js/Date.now)]
-    {:time {:elapsed (- t start-time)}
-     :mouse @mouse-state }))
-
-(defn sketch [start step draw]
-  (js/console.log "sketch")
-  (let [state (atom start)
-        renderer (renderer* (js/document.querySelector "#app")
-                            (draw start))]
-    (letfn [(render-loop [t]
-                         (.requestAnimationFrame js/window render-loop)
-                         (swap! state step (input))
-                         (.update renderer (draw @state)))]
-      (render-loop 0)))
-  :ok)
+  (:require zajal
+            there))
 
 (defn p [& body]
   (if (map? (first body))
-    (node "p" (first body) (rest body))
-    (node "p" {} body)))
+    (zajal/node "p" (first body) (rest body))
+    (zajal/node "p" {} body)))
 
 (defn em [& body]
   (if (map? (first body))
-    (node "em" (first body) (rest body))
-    (node "em" {} body)))
+    (zajal/node "em" (first body) (rest body))
+    (zajal/node "em" {} body)))
 
 (defn text [& s]
-  (text* (apply str s)))
+  (zajal/text (apply str s)))
 
 (def start {:click-count 0})
 
@@ -74,4 +27,4 @@
       (text (there/prand click-count))))
 
 (defonce test-sketch
-  (sketch start #'step #'draw))
+  (zajal/sketch start #'step #'draw))
